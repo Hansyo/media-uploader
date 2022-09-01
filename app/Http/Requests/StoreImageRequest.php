0ae1aaf036221ec51bbcd6817fe943422f669f3a
souta.hosoi <hosoi.sota.zw+intern@tut.jp>
2022-09-01 20:07:38 +0900

    [Add] 画像投稿 & 更新時のリクエストのバリデーションを追加

---
+++ app/Http/Requests/StoreImageRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreImageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // 登録されているユーザーなら誰でも可能
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title' => ['required', 'string', 'max:255'],
+            'description' => ['string', 'nullable'],
+            'file' => ['file', 'required_without:image_url'],
+            'iamge_url' => ['active_url', 'required_without:file'],
+        ];
+    }
+}
