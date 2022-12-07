000c7fd7630ccc178908ac25dcc6b0fed41e6fb2
Hansyo <hansyo.souti.1999@gmail.com>
2022-12-07 13:52:35 +0900

    fix: bug

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,10 +30,15 @@ class HomeController extends Controller
     {
         $page = $request->has('contents') ? $request->query('contents') : 1;
         $contents = Cache::remember(self::class.'_contents_'.$page, env("CACHE_TIME_SEC", 10), fn() => Content::latest()->paginate(env('PAGE_MAX_LIMIT', 20), ['*'], 'contents')->withQueryString());
-        $from =  $contents->last()->created_at;
-        $to = $contents->first()->created_at;
-        $images = Cache::remember(self::class.'_images_'.$page, env("CACHE_TIME_SEC", 10), fn() => Image::getBetweenCreated($from, $to));
-        $videos = Cache::remember(self::class.'_videos_'.$page, env("CACHE_TIME_SEC", 10), fn() => Video::getBetweenCreated($from, $to));
+        $from =  $contents->last()?->created_at;
+        if($from != null) {
+            $to = $contents->first()->created_at;
+            $images = Cache::remember(self::class.'_images_'.$page, env("CACHE_TIME_SEC", 10), fn() => Image::getBetweenCreated($from, $to));
+            $videos = Cache::remember(self::class.'_videos_'.$page, env("CACHE_TIME_SEC", 10), fn() => Video::getBetweenCreated($from, $to));
+        }else{
+            $images = collect();
+            $videos = collect();
+        }
 
         return view('home', compact('contents', 'images', 'videos'));
     }
